4dc26aab3d2f4e1f5b25024d7e5a072141fde2a9
Vladimir <vladimirsabo@yandex.ru>
2023-03-06 13:05:42 +0530

    feat: сменить запрос авторизации на @PreAuthorize в контроллере

--- src/main/java/demo/vlasabo/springsecuritydemo/controller/UserRestController.java
+++ src/main/java/demo/vlasabo/springsecuritydemo/controller/UserRestController.java
@@ -1,6 +1,7 @@
 package demo.vlasabo.springsecuritydemo.controller;
 
 import demo.vlasabo.springsecuritydemo.model.User;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -19,11 +20,13 @@ public class UserRestController {
     }
 
     @GetMapping
+    @PreAuthorize("hasAuthority('users:read')")
     public List<User> getAll() {
         return users ;
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('users:read')")
     public User getById(@PathVariable Long id) {
         return users.stream()
                 .filter(user -> Objects.equals(user.getId(), id))
@@ -31,12 +34,14 @@ public class UserRestController {
     }
 
     @PostMapping("/add")
+    @PreAuthorize("hasAuthority('users:write')")
     public User create(@RequestBody User user){
         users.add(user);
         return user;
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('users:write')")
     public void deleteById(@PathVariable Long id){
         users.removeIf(user -> Objects.equals(user.getId(), id));
     }
